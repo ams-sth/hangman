a42dd7690e394e3d255e3304fb7535a1f41ceb19
ams-sth <sthaamsh37@gmail.com>
2026-05-03 22:24:20 +1000

    Updated main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import tkinter as tk
-from src.hgui import HangmanGUI
+import sys
+import os
+sys.path.insert(0, os.path.abspath("src"))
+
+import customtkinter as ctk
+from gui.hgui import HangmanGUI
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    HangmanGUI(root)
+    root = ctk.CTk()
+    app = HangmanGUI(root)
     root.mainloop()
